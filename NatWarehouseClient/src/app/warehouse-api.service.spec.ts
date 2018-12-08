c76af585090ed47ab78d3079ad72c5e2ea8afcd3
Komáromi Balázs <b.komaromi21@gmail.com>
2018-12-08 14:32:51 +0100

    Fixed naming issue in warehouse-api.service.spec.ts

--- NatWarehouseClient/src/app/warehouse-api.service.spec.ts
+++ NatWarehouseClient/src/app/warehouse-api.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { WareouseApiService } from './warehouse-api.service';
+import { WarehouseApiService } from './warehouse-api.service';
 
 describe('WareouseApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: WareouseApiService = TestBed.get(WareouseApiService);
+    const service: WarehouseApiService = TestBed.get(WarehouseApiService);
     expect(service).toBeTruthy();
   });
 });
